c65c13437b02730fc3a132b02dfdbd1358999872
Anthony Sim <anthonyysim@gmail.com>
2021-02-08 12:01:05 -0800

    able to get Repo Model

--- helpers/top25.js
+++ helpers/top25.js
@@ -1,11 +1,12 @@
 const axios = require('axios');
-// const Repo = require('../../database/index.js')
+const Repo = require('../database/index.js')
 
 // request list of all items in database.
 // sort items from largest to smallest.
 // grab the first 25 then send it back to the client.
 
 const top25 = (callback) => {
+  console.log(Repo)
   console.log('you are getting something back!')
   // Model.find({}).then(data => console.log(data))
 }
